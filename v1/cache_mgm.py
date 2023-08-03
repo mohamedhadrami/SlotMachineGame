d1cff0e53d469bc1da0672069ef18829e5f7e35f
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2023-08-02 21:10:30 -0500

    enhanced parsing of saved games and added logic for loading games

--- v1/cache_mgm.py
+++ v1/cache_mgm.py
@@ -18,8 +18,8 @@ def save_game_data(path: os.path,
         pickle.dump(game_data, file)
 
 def load_game_data(game_path: os.path) -> dict:
-    if os.path.exists(game_path):
-        path = game_path
+    if os.path.exists(CACHE_PATH + game_path):
+        path = CACHE_PATH + game_path
         with open(path, "rb") as f:
             game_data = pickle.load(f)
             return game_data
@@ -34,6 +34,6 @@ def check_cache() -> list:
         saved_game = os.listdir(CACHE_PATH)
     saved_game_data = {}
     for file in saved_game:
-        data = load_game_data(CACHE_PATH + file)
+        data = load_game_data(file)
         saved_game_data[file] = data
     return saved_game_data
--- v1/main.py
+++ v1/main.py
@@ -1,5 +1,6 @@
 
 import os, time, cache_mgm, random
+from datetime import datetime
 from Board import Board
 
 MAX_BET = 100
@@ -29,14 +30,17 @@ def get_deposit() -> int:
             print("Please enter a number.")
     return amount
 
-def display_previous_games(data: dict) -> None:
-    saved_games = list(data.keys())
-    i = 1
-    for game in saved_games:
-        print(f"{i}. {game}")
+def display_previous_games(data: dict) -> dict:
+    saved_games = {index: key for index, key in enumerate(data.keys(), start=1)}
+    for i, game in saved_games.items():
+        game_time_object = datetime.strptime(game, "%m%d%Y-%H%M%S.pickle")
+        game_date = game_time_object.strftime("%d-%m-%Y")
+        game_time = game_time_object.strftime("%H:%M:%S")
+        print(f"{i}. {game_date} @ {game_time}")
         print(f"\tWinnings: {data[game]['gain']}")
         print(f"\tRounds: {len(data[game]['rounds'])}")
-        i += 1
+    return saved_games
+    
 
 def spin(balance: float, bet: int) -> int:
     board = user_board.get_board()
@@ -80,18 +84,29 @@ def main(balance: int) -> None:
 
 
 if __name__=="__main__":
-    data = cache_mgm.check_cache()
-    display_previous_games(data)
-    '''
-    new_game_bool = input("Do you want to load a game? \
-                          \nIf so, input the number of the file. \
-                          \nOtherwise press ENTER.")
-    if new_game_bool:
-        main(balance)
-    else:
-        balance = get_deposit()
-        main(balance)
-    '''
-    balance = get_deposit()
-    main(balance)
+    try:
+        data = cache_mgm.check_cache()
+        enumarable_game_data = display_previous_games(data)
+        new_game_bool = input("Do you want to load a game? \
+                            \nIf so, input the number of the file. \
+                            \nOtherwise press ENTER. ")
+        while True:
+            if new_game_bool:
+                if new_game_bool.isdigit() and int(new_game_bool) <= len(data):
+                    game_data = cache_mgm.load_game_data(enumarable_game_data[int(new_game_bool)])
+                    user_board = game_data['board']
+                    print(user_board)
+                    break
+                else:
+                    print("Must be a number matching a game")
+            else:
+                print("nah its new")
+                break
+            new_game_bool = input("Try again: ")
+        '''
+        '''
+    except KeyboardInterrupt:
+        print("\n\nLeft before the game started")
+    
+
 
